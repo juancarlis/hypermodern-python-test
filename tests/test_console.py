507512b06838321edb958af8ed2b0fa10f3213e5
Juan I. Carlis <jicarlis@ad-cap.com.ar>
2022-07-12 17:10:23 -0300

    test: adding test coverage

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,12 +1,17 @@
 # tests/test_console.property
 
 import click.testing
+import pytest
 
-from hypermodern_python import console
+from hypermodern_python_test import console
 
 
-def test_main_succeds():
-    runner = click.testing.CliRunner()
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeds(runner):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
