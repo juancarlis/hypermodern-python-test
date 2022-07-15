81f869d95d6dd6335474d90b474d8cbcb9d03b26
Juan I. Carlis <jicarlis@ad-cap.com.ar>
2022-07-15 20:45:42 -0300

    test/feat: adding tests and completing code

--- src/hypermodern_python_test/wikipedia.py
+++ src/hypermodern_python_test/wikipedia.py
@@ -1,12 +1,19 @@
 # src/hypermodern-python/wikipedia.property
 
 import requests
+import click
 
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page():
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        return response.json()
+def random_page(language="en"):
+    url = API_URL.format(language=language)
+
+    try:
+        with requests.get(url) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message)
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,7 +1,9 @@
 # tests/test_console.property
 
 import click.testing
+from requests.api import request
 import pytest
+import requests
 
 from hypermodern_python_test import console
 
@@ -9,16 +11,6 @@ from hypermodern_python_test import console
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
-
-
-@pytest.fixture
-def mock_requests_get(mocker):
-    mock = mocker.patch('requests.get')
-    mock.return_value.__enter__.return_value.json.return_value = {
-            'title': 'Lorem Ipsum',
-            'extract': 'Lorem ipsum dolor sit amet',
-            }
-    return mock
 
 
 def test_main_succeds(runner, mock_requests_get):
@@ -46,3 +38,19 @@ def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception('Boom')
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
+
+def test_main_prints_message_on_request_error(runner, mock_requests_get):
+    mock_requests_get.side_effect = requests.RequestException
+    result = runner.invoke(console.main)
+    assert 'Error' in result.output 
+
+
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch('hypermodern_python_test.wikipedia.random_page')
+
+
+def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+    runner.invoke(console.main, ["--language=pl"])
+    mock_wikipedia_random_page.assert_called_with(language="pl")
